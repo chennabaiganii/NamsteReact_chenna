363e8f7d5792ce8928ec44c5ea1b7cb612c302e8
NAGARRO\banagachennarao <nagachennarao.baigani@nagarro.com>
2023-06-22 18:33:36 +0530

    episod1

---
+++ README.md
@@ -0,0 +1,1 @@
+# Hello this repository belongs to chenna baigani.
---
+++ dist/index.6bd02f5a.js
@@ -0,0 +1,22 @@
+/*
+<div>
+  <div>
+    <h1>This is h1 </h1>
+    <h1>This is h2 </h1>
+   </div>
+</div>
+*/ const heading = React.createElement("div", {
+    id: "hding",
+    xyz: "abc"
+}, React.createElement("div", {}, [
+    React.createElement("h1", {}, "This is h1"),
+    React.createElement("h1", {}, "This is h2")
+]));
+//const heading = React.createElement("h1" ,{id:"hding",xyz:"abc"} , "Hello world react");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
+console.log(heading);
+console.log(root);
+n;
+
+//# sourceMappingURL=index.6bd02f5a.js.map
